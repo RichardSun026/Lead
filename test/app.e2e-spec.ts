80b99e21ceaa5e7b1836dbd8f23d6a5848eeb0e1
Peter Nyman <a23petny@student.his.se>
2025-04-30 11:14:37 -0300

    github bug?

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -21,4 +21,7 @@ describe('AppController (e2e)', () => {
       .get('/')
       .expect(200)
       .expect(
-        'FitCoachAI is a c
+        'FitCoachAI is a conversational fitness coach. It will generate personalised 7‑day training and supplementation plans while safely guiding you toward your fitness goals. It is able to reason over a your’s context (equipment, constraints, health notes) and call auxiliary functions such as search or scheduling **within a single response**.',
+      );
+  });
+});
