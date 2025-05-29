6fca7ab323273f6210f5bbe7eeef1be55ed6f565
Peter Nyman <skovdepeter@gmail.com>
2025-05-29 11:27:18 -0300

    moved and cleaned

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,15 @@ import { Module } from '@nestjs/common';
 import { RedisModule } from '@nestjs-modules/ioredis';
 
 import { AppController } from './app.controller';
-import { ConversationController } from './clientRedis/conversation.controller';
-import { AgentController } from './agent/agent.controller';
+import { ConversationController } from './agent/clientRedis/conversation.controller';
+import { AgentController } from './agent/agentLogic/agent.controller';
 
 
 import { AppService } from './app.service';
-import { ConversationService } from './clientRedis/conversation.service';
-import { AgentService } from './agent/agent.service';
-import { OpenAiService } from './agentHelp/openai.service';
-import { PromptService } from './agentHelp/prompt.service';
+import { ConversationService } from './agent/clientRedis/conversation.service';
+import { AgentService } from './agent/agentLogic/agent.service';
+import { OpenAiService } from './agent/agentHelp/openai.service';
+import { PromptService } from './agent/agentHelp/prompt.service';
 
 @Module({
   imports: [
