b517497ea2a1850c1f56b78e67605d99a87b09fb
Peter Nyman <skovdepeter@gmail.com>
2025-05-29 11:59:57 -0300

    cleaned and moved

--- scripts/load_supabase.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Loads schema and seed data into a Supabase database
-psql "$SUPABASE_DB_URL" < Database/postgres.sql
-if [ -f Database/seed.sql ]; then
-  psql "$SUPABASE_DB_URL" < Database/seed.sql
-fi
