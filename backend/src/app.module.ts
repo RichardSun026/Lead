f6dd749ddaf0939b53bef801bbc8df53aff3be31
Peter Nyman <skovdepeter@gmail.com>
2025-05-29 15:25:22 -0300

    fixed the stuct

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,18 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { RedisModule } from '@nestjs-modules/ioredis';
 
 import { AppController } from './app.controller';
-import { ConversationController } from './agent/clientRedis/conversation.controller';
-import { AgentController } from './agent/agentLogic/agent.controller';
+import { ConversationController } from './clientRedis/conversation.controller';
+import { AgentController } from './agentLogic/agent.controller';
 
 import { AppService } from './app.service';
-import { ConversationService } from './agent/clientRedis/conversation.service';
-import { AgentService } from './agent/agentLogic/agent.service';
-import { OpenAiService } from './agent/agentHelp/openai.service';
-import { PromptService } from './agent/agentHelp/prompt.service';
+import { ConversationService } from './clientRedis/conversation.service';
+import { AgentService } from './agentLogic/agent.service';
+import { OpenAiService } from './agentHelp/openai.service';
+import { PromptService } from './agentHelp/prompt.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: 'backend/.env',
+    }),
+
     RedisModule.forRoot({
       url: process.env.REDIS_URL,
       type: 'single',
