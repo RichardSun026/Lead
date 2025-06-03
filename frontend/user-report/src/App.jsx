c7ea3d98d92e7b29b4a448b03e391eb27b07ecd8
Peter Nyman <skovdepeter@gmail.com>
2025-06-03 14:06:33 -0300

    deleted unnessary code.

--- frontend/user-report/src/App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div>
-      <h1>User Report</h1>
-      <p>This React app will display user reports pulled from the backend.</p>
-    </div>
-  );
-}
