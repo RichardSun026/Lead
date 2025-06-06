bd1c7d570e310febb7b62ebd6d5cbf2911f8441f
peter nyman <150950929+PeterReisNyman@users.noreply.github.com>
2025-06-06 12:55:15 -0300

    fix: serve SPA routes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,20 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.setGlobalPrefix('api');
+
+  const expressApp = app.getHttpAdapter().getInstance();
+  expressApp.get(/^\/(?!api|survey).*/, (req, res) => {
+    res.sendFile(
+      join(__dirname, '..', '..', 'frontend', 'site', 'dist', 'index.html'),
+    );
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
